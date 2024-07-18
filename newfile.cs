3b76a0c43ceb325fa1e2d9de28a289a7aeb8678a
DPD2010 <63023368+dpd2010@users.noreply.github.com>
2024-07-18 21:08:30 +0000

    starter

--- newfile.cs
+++ newfile.cs
@@ -1,2 +1,13 @@
-using system;
-Console.
+using Internal;
+using System;
+
+namespace c1
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine('hello world');
+        }
+    }
+}
